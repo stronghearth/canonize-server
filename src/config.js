3796861c18f1639a72d2374fef68a0fc98143d87
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-02-13 12:43:42 -0500

    added JWT expiry

--- src/config.js
+++ src/config.js
@@ -2,5 +2,6 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/canonize',
-    JWT_SECRET: process.env.JWT_SECRET || 'i-am-a-lich-mimic-hear-my-random-flute'
+    JWT_SECRET: process.env.JWT_SECRET || 'i-am-a-lich-mimic-hear-my-random-flute',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '3hrs'
 }

0dde708f0586bcc268cd4304597c9605c3eda50f
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-02-16 18:16:03 -0500

    initial adjust to deploy with postgres

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
-    DB_URL: process.env.DB_URL || 'postgresql://postgres@localhost/canonize',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/canonize',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/canonize-test",
     JWT_SECRET: process.env.JWT_SECRET || 'i-am-a-lich-mimic-hear-my-random-flute',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3hrs'
 }

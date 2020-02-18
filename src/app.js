9526854147d7911a1b9e07aa08c8054f9cf45817
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-02-18 11:32:57 -0500

    adjustment for deployed version

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ const morganOption = (NODE_ENV === 'production')
 app.use(morgan(morganOption))
 app.use(helmet())
 app.use(cors({
-    origin: 'https://canonize.now.sh'
+    origin: CLIENT_ORIGIN
 }))
 
 app.use('/api/characters', charactersRouter)
--- src/config.js
+++ src/config.js
@@ -4,5 +4,6 @@ module.exports = {
     DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/canonize',
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/canonize-test",
     JWT_SECRET: process.env.JWT_SECRET || 'i-am-a-lich-mimic-hear-my-random-flute',
-    JWT_EXPIRY: process.env.JWT_EXPIRY || '3hrs'
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '3hrs',
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://canonize.now.sh'
 }

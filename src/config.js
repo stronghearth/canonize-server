1032492482628eae22717dff7f3d95d7e0a3aede
Taylor Bradshaw <taylordyoung1@gmail.com>
2020-02-18 14:41:40 -0500

    implemented client origin

--- src/config.js
+++ src/config.js
@@ -5,5 +5,5 @@ module.exports = {
     TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || "postgresql://postgres@localhost/canonize-test",
     JWT_SECRET: process.env.JWT_SECRET || 'i-am-a-lich-mimic-hear-my-random-flute',
     JWT_EXPIRY: process.env.JWT_EXPIRY || '3hrs',
-    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://canonize.now.sh'
+    CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 }
